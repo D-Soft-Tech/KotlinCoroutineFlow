bf8858d8113c4f8421f2bde645175711b354356b
DSoftTech <81322397+DSoftTech@users.noreply.github.com>
2021-08-06 13:49:24 +0100

    Added dependencies to the gradle file in order to use lifecycle components, glide, retrofit and coroutines

--- KotlinCoroutineFlow/app/src/main/java/com/example/kotlinflow/MainActivity.kt
+++ KotlinCoroutineFlow/app/src/main/java/com/example/kotlinflow/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotlinflow
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.flow.flowOf
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var data: ArrayList<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        (0..1000000).forEach { data.add(it) }
+
     }
+
+    fun sendData() = flowOf(data)
 }
